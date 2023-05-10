09a3ff195dcfe0e2b5f3f84ef4de7356dc04c08d
Tinwuindin <86543334+Tinwuindin@users.noreply.github.com>
2023-05-09 20:19:20 -0600

    Update Interfaz.py
    buttons, background and more :D

--- AndriodApp/Interfaz.py
+++ AndriodApp/Interfaz.py
@@ -1,6 +1,77 @@
-import tkinter 
+import tkinter
+import cv2 as cv 
+from tkinter import filedialog
 import time
 
+
+Patient_main = tkinter.Tk()
+Patient_main.geometry("1280x720")
+Patient_main.resizable("false","false")
+
+def load_archive():
+    video_path = filedialog.askopenfilename()
+    video = cv.VideoCapture(video_path)
+
+height = 120
+
+Body_measure_condition = False
+
+#Show body measure widgets
+Info_body_measure = tkinter.Label(Patient_main, text="Actual values", font="Arial 16")
+Height_tag = tkinter.Label(Patient_main, text="Height: ", font="Arial 12")
+Height_value = tkinter.Label(Patient_main, text=height, font="Arial 12")
+
+#Posture button widgets
+Info_posture = tkinter.Label(Patient_main, text="Select a video to start", font="Arial 14")
+Load_button = tkinter.Button(Patient_main, text="Load", command=load_archive)
+
+def show_posture():
+    hide_body_measure()
+    #Info_posture = tkinter.Label(Patient_main, text="Select a video to start", font="Arial 14")
+    #Load_button = tkinter.Button(Patient_main, text="Load", command=load_archive)
+    Info_posture.place(x=280, y=100)
+    Load_button.place(x=500, y=100)
+
+def hide_posture():
+    Info_posture.place_forget()
+    Load_button.place_forget()
+
+
+def show_body_measure():
+    hide_posture()
+    #Info_body_measure = tkinter.Label(Patient_main, text="Actual values", font="Arial 16")
+    #Height_tag = tkinter.Label(Patient_main, text="Height: ", font="Arial 12")
+    #Height_value = tkinter.Label(Patient_main, text=str(height), font="Arial 12")
+    Info_body_measure.place(x=280, y=100)
+    Height_value.place(x=330, y=200)
+    Height_tag.place(x=280, y=200)
+
+def hide_body_measure():
+    Info_body_measure.place_forget()
+    Height_value.place_forget()
+    Height_tag.place_forget()
+
+Buttons_background = tkinter.Canvas(Patient_main, width=280, height=720)
+Buttons_background.place(x=0,y=0)
+
+Body_measure = tkinter.Button(Patient_main, text="Modify body measure as height, weight, etc", font="Arial 12", command=show_body_measure, width=25, height=2,wraplength=200)
+Posture_button = tkinter.Button(Patient_main, text="Posture analyzer", font="Arial 12",command=show_posture, width=25, height=2,wraplength=200)
+Walk_button = tkinter.Button(Patient_main, text="Step analyzer", font="Arial 12", width=25, height=2,wraplength=200)
+
+Body_measure.place(x = 20,y = 50)
+Posture_button.place(x = 20, y = 120)
+Walk_button.place(x = 20, y = 190)
+
+Buttons_background.create_rectangle(0, 0, 280, 720, fill="green")
+
+if Body_measure_condition == False:
+    hide_body_measure()
+    hide_posture()
+    Body_measure_condition = True
+
+Patient_main.mainloop()
+
+"""
 #Main screen definition
 Main = tkinter.Tk()
 Main.geometry("600x200")
@@ -23,13 +94,11 @@ def ReadPatient():
         Patient_selection.delete(0,tkinter.END)
     else:
         Tag3.pack_forget()
-        Tag4.pack()
         Patient_selection.pack_forget()
         boton1.pack_forget()
-        time.sleep(2)
+        Tag4.pack()
         Main.destroy()
         
- 
 #Definition of the button
 boton1 = tkinter.Button(Main, text="Entry", command=ReadPatient)
 boton1.pack(side="bottom")
@@ -40,3 +109,4 @@ Patient_selection.pack(side="bottom")
 Patient_selection.bind("<Return>", lambda event: ReadPatient())
 
 Main.mainloop()
+"""
